6c28ee1f0cc664f1ba9073da282646933bffc22e
rust <rustam66637@gmail.com>
2021-08-26 22:54:58 +0300

    Initial Commit

---
+++ MarvelLib/MarvelLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelLibApp.swift
+//  MarvelLib
+//
+//  Created by wozdabady on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
