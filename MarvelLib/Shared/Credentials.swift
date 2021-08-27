d60be536b095744d51999b0988dac0d19f16de6c
rust <rustam66637@gmail.com>
2021-08-27 19:23:42 +0300

    ref

--- MarvelLib/Shared/Credentials.swift
+++ MarvelLib/Shared/Credentials.swift
@@ -5,8 +5,6 @@
 //  Created by wozdabady on 26.08.2021.
 //
 
-let publicKey = "b95f798692c933dfeb3abb290d7f2096"
-let privateKey = "008b960383bc4c262736864aa34fe98c92edc516"
 
-//let publicKey = "your publicKey"
-//let privateKey = "your privateKey"
+let publicKey = "your publicKey"
+let privateKey = "your privateKey"
